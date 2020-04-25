a04d8041d2f0f11cf5f48bd3c26f28d69e9e80e7
Miguel Torrealba <mtorrealba@techk.cl>
2020-04-25 13:48:00 -0400

    Create component Logo

---
+++ src/components/Logo.svelte
@@ -0,0 +1,14 @@
+<style>
+  .logo {
+    display: flex;
+    justify-content: center;
+  }
+  #logo-img {
+    width: 8%;
+    background-color: white;
+  }
+</style>
+
+<div class="logo">
+  <img src="../../public/img/logo-virus.svg" alt="Trackovid Logo" id="logo-img">
+</div>
--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,4 +1,12 @@
 <script>
-  alert('comienzo de proyecto')
+  import Logo from '../components/Logo.svelte';
 </script>
 
+<style>
+  :global(body) {
+    background: rgb(27,27,31,1);
+    /* background: linear-gradient(17deg, rgba(64,64,69,1) 0%, rgba(27,27,31,1) 37%); */
+  }
+</style>
+
+<Logo />
