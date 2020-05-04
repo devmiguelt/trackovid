b3259f269d805eaa76980d36d2c44cb918ae42db
Miguel Torrealba <mtorrealba@techk.cl>
2020-05-03 20:57:19 -0400

    Add file

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { writable } from 'svelte/store';
+
+export const count_infected = writable(0);
+export const count_saved = writable(0);
+export const count_dead = writable(0);
+export const count_tests = writable(0);
+export const country = writable('');
+export const showCountry = writable(false);
