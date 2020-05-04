c7839c09d7feb70ec28f132239b7c937ef4a9f99
Miguel Torrealba <mtorrealba@techk.cl>
2020-05-03 22:00:04 -0400

    Change test

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,6 @@ module.exports = {
         }
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-      {
         test: /\.(png|jp(e*)g|svg)$/,  
         use: [{
             loader: 'url-loader',
