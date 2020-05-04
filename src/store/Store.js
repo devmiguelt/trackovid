a8ebd71cd3693ce2c4c69da727d3e06df982bead
Miguel Torrealba <mtorrealba@techk.cl>
2020-05-03 20:56:46 -0400

    Remove file

--- src/store/Store.js
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const count_infected = writable(0);
-export const count_saved = writable(0);
-export const count_dead = writable(0);
-export const count_tests = writable(0);
-export const country = writable('');
-export const showCountry = writable(false);
